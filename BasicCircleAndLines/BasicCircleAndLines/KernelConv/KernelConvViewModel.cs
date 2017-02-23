a58316690642e8e57f9b1660f3c3a890e2e9b14d
Frison B Alexander <fri.b.alex@gmail.com>
2017-02-22 19:24:55 -0500

    Time to think about an approach..
    Found the source of the "collection mismatch" bug, turns out that
    running a second thread in the background to modify the collection is
    not going to work. Instead, the Application.Current.Dispatcher has to be
    told to que up work that will modify the collection.
    
    The problem now is, in order to scroll the ListBox after insertion of
    new item the ViewModel needs a reference to the ListBox. But taking a
    reference from the VIewModel to the view would make it harder to test
    the ViewModel..
    
    Need to now experiment with different approaches to see where this code
    should go..
    1) In the "code behind" file for the window.
    2) In the xaml. Perhaps there is some parameter that I could set.

---
+++ BasicCircleAndLines/BasicCircleAndLines/KernelConv/KernelConvViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+using System.Threading;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+
+namespace BasicCircleAndLines.KernelConv
+{
+    //TODO: There is a class from prisim 5 to inherit from to make change tracking easier?
+    public class KernelConvViewModel
+    {
+        
+        public ObservableCollection<KernelWindow> KernelBuffer { get; set; } = new ObservableCollection<KernelWindow>();
+
+        public KernelConvViewModel()
+        {
+            Task.Run(AddStuffToBuffer);
+        }
+        
+        private async Task AddStuffToBuffer()
+        {
+            var r = new Random();
+
+            Func<byte> b = () => Convert.ToByte(r.Next(0, 255));
+            Func<Brush> color = () => new SolidColorBrush(Color.FromRgb(b(), b(), b()));
+            var MAX_ITERATION = 100;
+            var iteration = 0;
+            do
+            {
+                var frame = new KernelWindow
+                {
+                    Color = color(),
+                    Height = r.NextDouble() * 100,
+                    Width = r.NextDouble() * 100,
+                    Rotation  = r.NextDouble() * 100
+                };
+
+                Application.Current.Dispatcher.InvokeAsync(() => KernelBuffer.Add(frame));
+                
+
+                await Task.Delay(800);
+
+            } while (iteration++ < MAX_ITERATION);            
+        }
+    }
+
+    public class KernelWindow
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+        public double Rotation { get; set; }
+
+        public Brush Color { get; set; }
+    }
+}
